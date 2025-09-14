fbbf08de4397e2f4e35a09076d39cfee47110c9e
Ani861 <gkaniruddha26@gmail.com>
2025-09-14 14:52:41 +0530

    Updated

--- Resume_Analyzer.py
+++ Resume_Analyzer.py
@@ -18,6 +18,11 @@ try:
 except LookupError:
     nltk.download('punkt')
 try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+
+try:
     nltk.data.find('taggers/averaged_perceptron_tagger')
 except LookupError:
     nltk.download('averaged_perceptron_tagger')
